e992d4a85f6f21b31e930faae44de4b4e168a7ab
mahmoudgawish <mahmoud.gawish@altibbi.com>
2021-06-01 13:37:39 +0200

    add spredsheet

--- src/test/java/php/travels/TestRgistrationPage.java
+++ src/test/java/php/travels/TestRgistrationPage.java
@@ -7,19 +7,22 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import utilites.SpreedSheet;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 
 
 public class TestRgistrationPage {
 	public WebDriver driver;
+
 	String fnam = "Nashwa";
 	String lsname = "Nouh";
 	String tel = "123456";
-	String eml = "d4@gmail.com";
-	String Paswrd= "1234567"; 
-	String conpw = "1234567";
+	//String email = "d7dd@gmail.com";
+	//String Password= "1234567";
+	String repassword = "1234567";
 
 	@BeforeClass
 	public void openUrl() {
@@ -35,9 +38,20 @@ public class TestRgistrationPage {
 	@Test
 	public void regTest()
 	{
+		String fileName = "resourcse/signup.xlsx";
+		var spreadsheet = new SpreedSheet(new File(fileName));
+		spreadsheet.switchToSheets("user1");
+		int row = 1;
+		String firstname = spreadsheet.GetCellData("fnam",row);
+		String lastName = spreadsheet.GetCellData("lsname",row);
+		String telephone = spreadsheet.GetCellData("tel",row);
+		String mail = spreadsheet.GetCellData("email",row);
+		String password = spreadsheet.GetCellData("Password",row);
+		String repassword = spreadsheet.GetCellData("repassword",row);
+
 		RegistrationClass regObj = new RegistrationClass(driver);
-		regObj.registration(fnam, lsname, tel, eml,Paswrd,conpw);
-		Assert.assertEquals(regObj.getResultText(), "Hi," + " " + fnam + " "+ lsname);
+		regObj.registration(firstname, lastName, telephone, mail,password,repassword);
+		Assert.assertEquals(regObj.getResultText(), "Hi," + " " + firstname + " "+ lastName);
 	}
 
 	@AfterClass
